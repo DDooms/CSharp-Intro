482ca1b7977874512b6676cf044fdd84be4ea862
DDooms <esyuleyman94@gmail.com>
2023-07-27 22:59:43 +0300

    .

---
+++ cSharpIntro/TestingClasses.cs
@@ -0,0 +1,8 @@
+﻿namespace cSharpIntro;
+
+public class TestingClasses
+{
+    public string test { get; set; }
+    public int I { get; private set; }
+    private Dictionary<int, string> test2 = new Dictionary<int, string>();
+}
